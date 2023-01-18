ff6967d0c8988a92dff5702f974041f626292af4
Ушков Вячеслав Сергеевич <v.ushkov@bio-smart.ru>
2023-01-18 11:32:47 +0500

    Added disconnected log

--- netcalcserver.pro
+++ netcalcserver.pro
@@ -10,6 +10,7 @@ CONFIG -= app_bundle
 
 SOURCES += \
         main.cpp \
+        mydisconnected.cpp \
         newconnection.cpp \
         timestamp.cpp
 
@@ -19,5 +20,6 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 HEADERS += \
+    mydisconnected.h \
     newconnection.h \
     timestamp.h
--- newconnection.cpp
+++ newconnection.cpp
@@ -1,9 +1,11 @@
 #include <QTextStream>
 #include <QTcpSocket>
+#include <QString>
 #include "newconnection.h"
 #include "timestamp.h"
+#include "mydisconnected.h"
 
-QTcpSocket *socket;
+
 
 MyNewConnection::MyNewConnection(QObject *parent) : QObject(parent)
 {
@@ -12,10 +14,14 @@ MyNewConnection::MyNewConnection(QObject *parent) : QObject(parent)
 void MyNewConnection::myNewConn(QTcpServer *conn)
     {
     // Получаем сокет вновь подключенного клиента
-          socket = conn->nextPendingConnection();
-          socket->peerAddress();
+          QTcpSocket *socket = conn->nextPendingConnection();
+          QString ipString = socket->peerAddress().toString();
 
 
     // Выводим в лог информацию о новом подключении
-          QTextStream(stdout) << getTimeStamp() << " New connection: " << socket->peerAddress().toString() << "\n";
+          QTextStream(stdout) << getTimeStamp() << " New connection: " << ipString << "\n";
+
+          MyDisconnected *myDisconnObj = new MyDisconnected;
+
+          QObject::connect(socket, &QTcpSocket::disconnected, [=]{ myDisconnObj->myDist(ipString); });
     };
